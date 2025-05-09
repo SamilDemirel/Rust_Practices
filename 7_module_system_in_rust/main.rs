6dadd3ff50aed483afb433a5599df2fbbe5a4767
SamilDemirel <samildourden@gmail.com>
2025-05-09 23:14:40 +0300

    module_system_in_rust added

---
+++ 7_module_system_in_rust/main.rs
@@ -0,0 +1,17 @@
+/* Rust Module System
+    - Package
+        - Crates (binary or library) main.rs is the root crate of the package
+            - Modules    
+        
+**if we create a new package on terminal with :
+    /cargo new --lib my_package 
+    package will create with lib.rs in src folder. not with main.rs and a test module automaticly added
+ */
+
+
+
+fn main(){
+
+
+
+}
